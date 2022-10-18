8b74ce15dd643f55ecde9e9d67c772b54373829b
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-10-18 18:43:16 -0500

    commit tree 0227abd1e4af1d91bee6c550908b370fb6de969d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+//document.addEventListener("DOMContentLoaded", function()){
+
+//}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+   let idText= document.querySelector('p')
+   idText.textContent ='This is really cool!'
+  });
+
+
+
+
+ 
